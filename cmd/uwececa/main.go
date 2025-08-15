85a73ad7db1ee3279cc3935b6b22a71f7aab5d5d
foehammer127 <foehammer@disroot.org>
2025-08-15 00:05:50 -0500

    Implement command line.

---
+++ cmd/uwececa/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"uwece.ca/app/config"
+	"uwece.ca/app/db"
+	"uwece.ca/app/shutdown"
+	"uwece.ca/app/site"
+)
+
+func main() {
+	if os.Getenv("SLOG_DEBUG") == "1" {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		slog.Error("error running app", "error", err)
+		os.Exit(1)
+	}
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGTERM, os.Interrupt)
+	<-s
+
+	shutdown.Shutdown(10 * time.Second)
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.Load(ctx)
+	if err != nil {
+		return fmt.Errorf("config load error: %w", err)
+	}
+
+	db, err := db.New(cfg.DB.Location)
+	if err != nil {
+		return fmt.Errorf("error opening db: %w", err)
+	}
+
+	mainsite := site.New(cfg, db)
+
+	startServer(mainsite.Routes(), cfg.Core.Addr)
+
+	return nil
+}
+
+func startServer(h http.Handler, addr string) {
+	s := &http.Server{
+		ReadTimeout:       1 * time.Second,
+		WriteTimeout:      1 * time.Second,
+		IdleTimeout:       30 * time.Second, //nolint:mnd // fine
+		ReadHeaderTimeout: 2 * time.Second,  //nolint:mnd // fine
+		Handler:           h,
+		Addr:              addr,
+	}
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("failed to start http server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	shutdown.AddFunc(func() {
+		sCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := s.Shutdown(sCtx); err != nil {
+			slog.Warn("failed to stop http server", "error", err)
+		}
+
+		slog.Debug("stopped http server")
+	})
+
+	slog.Info("started http server", "addr", addr)
+}
--- config/config.go
+++ config/config.go
@@ -1,9 +1,30 @@
 package config
 
+import (
+	"context"
+
+	envconfig "github.com/sethvargo/go-envconfig"
+)
+
 type Config struct {
-	Core Core
+	Core Core `env:",prefix=UWECECA_"`
+	DB   DB   `env:",prefix=UWECECA_DB_"`
 }
 
 type Core struct {
-	Development bool
+	Development bool   `env:"DEVELOPMENT,default=false"`
+	Addr        string `env:"ADDR,default=localhost:3000"`
+}
+
+type DB struct {
+	Location string `env:"LOCATION,default=db.sqlite3"`
+}
+
+func Load(ctx context.Context) (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process(ctx, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
