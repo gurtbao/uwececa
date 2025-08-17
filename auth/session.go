d2a4efdce7d1cb953e64466654d6e59eebade802
foehammer127 <foehammer@disroot.org>
2025-08-17 15:25:49 -0500

    auth: Add session implementation and password hashing.
    Add a session implementation using db based session tokens
    (models.Session). Add password hashing and verification with argon2ID.
    Passwords are stored in the database in an encoded format.

---
+++ auth/session.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	cookieName  = "uwececa_session_token_v1"
+	expiryHours = 48
+	tokenBytes  = 32
+)
+
+type Session struct {
+	Token  string
+	Expiry time.Time
+}
+
+// Create a new session token (a hex encoded string of len tokenBytes).
+func NewSession() Session {
+	token := make([]byte, tokenBytes)
+	_, err := rand.Read(token)
+	if err != nil {
+		panic(fmt.Sprintf("error reading bytes for session token: %v", err))
+	}
+
+	return Session{
+		Token:  hex.EncodeToString(token),
+		Expiry: time.Now().Add(expiryHours * time.Hour),
+	}
+}
+
+// Send session token to user's browser.
+func AddSession(w http.ResponseWriter, s Session) {
+	cookie := &http.Cookie{
+		Name:   cookieName,
+		Value:  s.Token,
+		Quoted: false,
+
+		SameSite: http.SameSiteStrictMode,
+		Path:     "/",
+		Secure:   true,
+		Expires:  s.Expiry,
+	}
+
+	http.SetCookie(w, cookie)
+}
+
+func GetSession(r *http.Request) (Session, bool) {
+	cookie, err := r.Cookie(cookieName)
+	if err != nil {
+		return Session{}, false
+	}
+
+	s := Session{
+		Token:  cookie.Value,
+		Expiry: cookie.Expires,
+	}
+
+	return s, true
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module uwece.ca/app
 go 1.24.5
 
 require (
+	github.com/go-chi/chi/v5 v5.2.2
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/matthewhartstonge/argon2 v1.3.4
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/sethvargo/go-envconfig v1.3.0
 	github.com/stretchr/testify v1.10.0
@@ -11,7 +13,8 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
