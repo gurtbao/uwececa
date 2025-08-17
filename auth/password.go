fd1dbf44acc447fdafbb06c49331788a9960770e
foehammer127 <foehammer@disroot.org>
2025-08-17 15:25:49 -0500

    auth: Add session implementation and password hashing.
    Add a session implementation using db based session tokens
    (models.Session). Add password hashing and verification with argon2ID.
    Passwords are stored in the database in an encoded format.

---
+++ auth/password.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"fmt"
+
+	"github.com/matthewhartstonge/argon2"
+)
+
+var argon = argon2.DefaultConfig()
+
+func HashPassword(password string) string {
+	encoded, err := argon.HashEncoded([]byte(password))
+	if err != nil {
+		panic(fmt.Sprintf("password hashing error: %v", err))
+	}
+
+	return string(encoded)
+}
+
+func VerifyPassword(password, hash string) (bool, error) {
+	ok, err := argon2.VerifyEncoded([]byte(password), []byte(hash))
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+func MustVerifyPassword(password, hash string) bool {
+	ok, err := VerifyPassword(password, hash)
+	if err != nil {
+		panic(fmt.Sprintf("password verification error: %v", err))
+	}
+
+	return ok
+}
