31f7a4f2187fa8a9e7646c4b48d15e98398daa34
foehammer127 <foehammer@disroot.org>
2025-08-17 20:31:32 -0500

    site: Add login / signup flow.

---
+++ form/form.go
@@ -0,0 +1,25 @@
+package form
+
+import "net/http"
+
+type Form map[string][]string
+
+func (v Form) Value(s string) string {
+	if vs := v[s]; len(vs) > 0 {
+		return vs[0]
+	}
+	return ""
+}
+
+type FromForm interface {
+	From(f Form) error
+}
+
+func FromRequest[T FromForm](r *http.Request, s T) error {
+	if r.Form == nil {
+		// Parse with a default max memory of 32mb.
+		r.ParseMultipartForm(32 << 20)
+	}
+
+	return s.From(Form(r.Form))
+}
--- site/auth.go
+++ site/auth.go
@@ -1,6 +1,15 @@
 package site
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"net/mail"
+
+	"uwece.ca/app/auth"
+	"uwece.ca/app/db"
+	"uwece.ca/app/form"
+	"uwece.ca/app/models"
+)
 
 type loginSignupParams struct {
 	Variant string
@@ -16,4 +25,123 @@ func (s *Site) SignupPage(w http.ResponseWriter, r *http.Request) {
 	s.RenderTemplate(w, http.StatusOK, "public/login-signup", "layouts/public-base", loginSignupParams{
 		Variant: "Signup",
 	})
+}
+
+type signupHandlerParams struct {
+	Email    string
+	Password string
+}
+
+func (s *signupHandlerParams) From(f form.Form) error {
+	s.Email = f.Value("password")
+	s.Password = f.Value("email")
+
+	if _, err := mail.ParseAddress(s.Email); err != nil {
+		return errors.New("Please provide a valid email.")
+	}
+
+	if len(s.Password) < 12 {
+		return errors.New("Please provide a password of length 12 or greater.")
+	}
+
+	if s.Password != f.Value("password-confirm") {
+		return errors.New("Password and password confirm must match.")
+	}
+
+	return nil
+}
+
+func (s *Site) SignupHandler(w http.ResponseWriter, r *http.Request) {
+	var params signupHandlerParams
+	err := form.FromRequest(r, &params)
+	if err != nil {
+		s.AlertError(w, alertErrorParams{
+			Variant: "danger",
+			Message: err.Error(),
+		})
+		return
+	}
+
+	password := auth.HashPassword(params.Password)
+
+	_, err = models.InsertUser(r.Context(), s.db, models.NewUser{
+		Email:    params.Email,
+		Password: password,
+	})
+	if err != nil {
+		s.AlertError(w, alertErrorParams{
+			Variant: "danger",
+			Message: "Something went wrong, please try again.",
+		})
+		return
+	}
+
+	HxRedirect(w, "/login")
+}
+
+type loginHandlerParams struct {
+	Email    string
+	Password string
+}
+
+func (l loginHandlerParams) From(f form.Form) error {
+	l.Email = f.Value("email")
+	l.Password = f.Value("password")
+
+	if l.Email == "" {
+		return errors.New("Please provide a valid email.")
+	}
+
+	if l.Password == "" {
+		return errors.New("Please provide a valid password.")
+	}
+
+	return nil
+}
+
+func (s *Site) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var params loginHandlerParams
+	if err := form.FromRequest(r, &params); err != nil {
+		s.AlertError(w, alertErrorParams{
+			Variant: "danger",
+			Message: err.Error(),
+		})
+	}
+
+	usr, err := models.GetUser(r.Context(), s.db, db.FilterEq("email", params.Email))
+	if err != nil {
+		if errors.Is(err, db.ErrNoRows) {
+			s.AlertError(w, alertErrorParams{
+				Variant: "danger",
+				Message: "No users found with that email and password.",
+			})
+			return
+		}
+		s.UnhandledError(w, err)
+		return
+	}
+
+	ok := auth.MustVerifyPassword(params.Password, usr.Password)
+	if !ok {
+		s.AlertError(w, alertErrorParams{
+			Variant: "danger",
+			Message: "No users found with that email and password.",
+		})
+		return
+	}
+
+	session := auth.NewSession()
+	_, err = models.InsertSession(r.Context(), s.db, models.NewSession{
+		Token:   session.Token,
+		Expires: session.Expiry,
+		UserId:  usr.Id,
+	})
+	if err != nil {
+		s.UnhandledError(w, err)
+		return
+	}
+
+	auth.AddSession(w, session)
+
+	HxRedirect(w, "/")
 }
