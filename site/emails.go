7e8a6cbefffc7274685242fc27c400795496c0b3
foehammer127 <foehammer@disroot.org>
2025-08-19 17:04:29 -0500

    site: Add mailer / SendVerificationEmail.

---
+++ site/emails.go
@@ -0,0 +1,42 @@
+package site
+
+import (
+	"fmt"
+	"html/template"
+
+	"uwece.ca/app/auth"
+	"uwece.ca/app/email"
+)
+
+func (s *Site) SendVerificationEmail(addr, name string, token auth.Token) error {
+	params := struct {
+		Name string
+		Link template.HTML
+	}{
+		Name: name,
+		Link: template.HTML(token),
+	}
+
+	text, err := s.templates.ExecutePlainString("emails/verification.txt", params)
+	if err != nil {
+		return fmt.Errorf("error rendering email text template: %w", err)
+	}
+
+	html, err := s.templates.ExecutePlainString("emails/verification", params)
+	if err != nil {
+		return fmt.Errorf("error rendering email html template: %w", err)
+	}
+
+	err = s.mailer.SendMessage(email.Message{
+		To:       addr,
+		Name:     name,
+		Subject:  "UWECECA - Verification.",
+		TextBody: text,
+		HtmlBody: html,
+	})
+	if err != nil {
+		return fmt.Errorf("error sending email: %w", err)
+	}
+
+	return nil
+}
