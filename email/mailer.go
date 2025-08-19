531b9e585fd8e61dc87ee3efd063ddb6785d9e94
foehammer127 <foehammer@disroot.org>
2025-08-19 16:34:04 -0500

    email: Add mailer and email send abstraction.

---
+++ email/mailer.go
@@ -0,0 +1,43 @@
+package email
+
+import (
+	"gopkg.in/gomail.v2"
+	"uwece.ca/app/config"
+)
+
+type Mailer struct {
+	mailer *gomail.Dialer
+	cfg    *config.Config
+}
+
+func NewMailer(cfg *config.Config) *Mailer {
+	m := cfg.Mailer
+	d := gomail.NewDialer(m.Host, m.Port, m.Username, m.Password)
+
+	return &Mailer{
+		mailer: d,
+		cfg:    cfg,
+	}
+}
+
+type Message struct {
+	To       string
+	Name     string
+	Subject  string
+	TextBody string
+	HtmlBody string
+}
+
+func (m *Mailer) SendMessage(me Message) error {
+	message := gomail.NewMessage()
+
+	message.SetAddressHeader("From", m.cfg.Mailer.FromAddress, "UWECECA Team")
+	message.SetHeader("Subject", me.Subject)
+	message.SetAddressHeader("To", me.To, me.Name)
+	message.SetBody("text/plain", me.TextBody)
+	if me.HtmlBody != "" {
+		message.AddAlternative("text/html", me.HtmlBody)
+	}
+
+	return m.mailer.DialAndSend(message)
+}
