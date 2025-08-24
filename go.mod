fd50a73e8dca2e35dd352f5d2ad97536b3ff96dd
foehammer127 <foehammer@disroot.org>
2025-08-24 01:17:49 -0500

    refactor: Use service architecture.

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/sethvargo/go-envconfig v1.3.0
 	github.com/stretchr/testify v1.10.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -17,6 +18,5 @@ require (
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ services/blog.go
@@ -0,0 +1,69 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"uwece.ca/app/db"
+	"uwece.ca/app/models"
+)
+
+var ErrSubdomainNotAvailable = errors.New("subdomain not available")
+
+type BlogService struct {
+	db *db.DB
+}
+
+type BlogNewRequest struct {
+	Name string
+	Year int
+}
+
+func (b BlogNewRequest) Validate() error {
+	filteredName := strings.Map(func(r rune) rune {
+		if r < 'a' || r > 'z' {
+			return -1
+		}
+
+		return r
+	}, b.Name)
+	if b.Name != filteredName {
+		return errors.New("Please submit a valid name (a-z).")
+	}
+
+	if len(b.Name) == 0 || len(b.Name) >= 35 {
+		return fmt.Errorf("Please submit a valid name (1 - 35 characters in length).")
+	}
+
+	if b.Year < 24 || b.Year > 30 {
+		return fmt.Errorf("Please submit a valid year. (24 - 30).")
+	}
+
+	return nil
+}
+
+func (s *BlogService) New(ctx context.Context, req BlogNewRequest, usrID int) error {
+	if err := req.Validate(); err != nil {
+		return fmt.Errorf("%w: %v", ErrValidationFailed, err)
+	}
+
+	subdomain := fmt.Sprintf("%s.%d", req.Name, req.Year)
+
+	_, err := models.InsertSite(ctx, s.db, models.NewSite{
+		UserId:      usrID,
+		Subdomain:   subdomain,
+		Navbar:      "[Home](/)",
+		HomeContent: "# Hello World \n Hola Mundo.",
+	})
+	if err != nil {
+		if errors.Is(err, db.ErrUnique) {
+			return ErrSubdomainNotAvailable
+		}
+
+		return fmt.Errorf("error inserting website into database: %w", err)
+	}
+
+	return nil
+}
